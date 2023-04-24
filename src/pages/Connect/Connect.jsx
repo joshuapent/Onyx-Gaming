ea5bc7ee92942b9a70a07d1629f531f2b4527242
piquant95 <piquant95@yahoo.com>
2023-04-24 16:07:19 -0400

    connect page image added

--- src/pages/Connect/Connect.jsx
+++ src/pages/Connect/Connect.jsx
@@ -1,7 +1,17 @@
-export default function Connect() {  
-    return (
-      <div className="connect">
-        <span><img src="https://i.imgur.com/A48hlBy.png" alt="Gamer Swipe" srcset="" /></span>
-      </div>
-    );
-  }
+import "./Connect.css";
+
+export default function Connect() {
+  return (
+    <div className="connect">
+      <span>
+        <img
+          className="swipe"
+          src="https://i.imgur.com/A48hlBy.png"
+          alt="Gamer Swipe"
+          srcset=""
+        />
+      </span>
+      
+    </div>
+  );
+}
