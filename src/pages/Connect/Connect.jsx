f64813dfb0d7856ba57cebf72d3042c826056459
piquant95 <piquant95@yahoo.com>
2023-04-24 14:59:28 -0400

    altered text color

--- src/pages/Connect/Connect.jsx
+++ src/pages/Connect/Connect.jsx
@@ -1,7 +1,7 @@
 export default function Connect() {  
     return (
       <div className="connect">
-        <h1>Connect</h1>
+        <span><img src="https://i.imgur.com/A48hlBy.png" alt="Gamer Swipe" srcset="" /></span>
       </div>
     );
   }
