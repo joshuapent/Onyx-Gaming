674d8cacebe235a4ec87b8ad274dc9994763d161
joshuapent <joshdavidpent@gmail.com>
2023-04-24 10:06:37 -0500

    Added to models

--- config/database.js
+++ config/database.js
@@ -7,3 +7,11 @@ const db = mongoose.connection;
 db.on('connected', function () {
   console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
+
+db.on('error', (error) => {
+  console.log('MongoDB experienced a connection error', error)
+})
+
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected...')
+})
