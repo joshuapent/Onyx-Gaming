ae42a5de489ed8f86ec5f0d0071c75fadc194769
joshuapent <joshdavidpent@gmail.com>
2023-04-25 12:28:29 -0500

    Models updated

--- models/chat.js
+++ models/chat.js
@@ -1,19 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
-const chatSchema = new Schema({
-    userA: {
+const messageSchema = new Schema({
+    sender_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
-    userB: {
+    recipient_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    text: {
+            type: String, 
+            required: true
+    }
   }, {
     timestamps: true,
   }
 );
+
+const chatSchema = new Schema({
+    users: 
+        [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        }],
+    messages: [messageSchema]
+  }, {
+    timestamps: true,
+  }
+);
+
+
 
 module.exports = mongoose.model('Chat', chatSchema);
