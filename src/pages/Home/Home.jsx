46560686a32ce1e712003896ee296ed33c276e6b
Piquant95 <piquant95@yahoo.com>
2023-04-25 14:07:17 -0400

    logo img added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,22 @@
+import "./Home.css";
 export default function Home() {  
     return (
       <div className="home">
-        <h1>Home</h1>
+        <div className="paralax">
+          <div className="paralax-item p-item1"></div>
+          <div className="paralax-item p-item2"></div>
+          <div className="paralax-item p-item3"></div>
+          <div className="paralax-item p-item4"></div>
+        </div>
+        <div className="container">
+          <div className="content-header">
+            <h1>Welcome to Onyx Gaming!</h1>
+            <h2>Got Game?</h2>
+          </div>
+          <div className="content">
+            <img src="https://i.imgur.com/e6gOX8t.png" alt="GamerCentral" srcset="" />
+          </div>
+        </div>
       </div>
     );
   }
