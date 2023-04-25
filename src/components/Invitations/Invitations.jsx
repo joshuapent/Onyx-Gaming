16791bc80e9944e7f10d786a91f46e3066a19ce2
joshuapent <joshdavidpent@gmail.com>
2023-04-25 12:51:36 -0500

    Added functionality for a layered connections page

---
+++ src/components/Invitations/Invitations.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./Invitations.css";
+
+function Invitations({ user }) {
+  return (
+    <div className="Invitations">
+      <h1>Welcome to Invitations</h1>
+    </div>
+  );
+}
+
+export default Invitations;
--- src/pages/Connections/Connections.jsx
+++ src/pages/Connections/Connections.jsx
@@ -1,11 +1,23 @@
 import ChatPage from "../../components/ChatPage/ChatPage";
+import { Routes, Route, Link } from 'react-router-dom';
+import './Connections.css';
+import Invitations from "../../components/Invitations/Invitations";
+
 
 export default function Connections({user}) {  
     return (
       <div className="connections">
         <h1>Connections</h1>
-        <div>
-            <ChatPage user={user}/>
+        <div className="connections-links">
+            <Link className="connections-link" to="chat/">Chat</Link>
+            &nbsp; | &nbsp;
+            <Link className="connections-link" to="invitations/">Invitations</Link>
+        </div>
+        <div className="connections-routes">
+            <Routes>
+                <Route path="/chat" element={< ChatPage user={user}/>}></Route>
+                <Route path="/invitations" element={< Invitations user={user} />}></Route>
+            </Routes>
         </div>
       </div>
     );
