65352f21dfebdbfe265b2b111eff887df8e9341e
joshuapent <joshdavidpent@gmail.com>
2023-04-26 10:16:25 -0500

    Added some css for the connect card

--- src/pages/Connect/Connect.jsx
+++ src/pages/Connect/Connect.jsx
@@ -1,7 +1,24 @@
+import './Connect.css'
+
 export default function Connect() {  
     return (
       <div className="connect">
-        <h1>Connect</h1>
+        <div className="connect-header">
+          <h1>Connect</h1>
+        </div>
+
+        <div className="connect-card">
+          <div className="connect-card-nameplate">
+            <h1>(insert name)</h1>
+          </div>
+          <div className="connect-card-body">
+            <h1>body</h1>
+          </div>
+          <div className="connect-card-footer">
+            <h1>(footer Options)</h1>
+          </div>
+        </div>
+
         <div className='wrapper'>
         <div className='top-panel'></div>
         <div className='bottom-panel'></div>
