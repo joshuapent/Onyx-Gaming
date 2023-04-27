6c4fa5766dc9b58a49909446ac6619cde8057893
joshuapent <joshdavidpent@gmail.com>
2023-04-27 08:59:33 -0500

    One card at a time functionality working

--- src/components/ConnectCard/ConnectCard.jsx
+++ src/components/ConnectCard/ConnectCard.jsx
@@ -1,28 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
 
-function ConnectCard({user}) {
-
-
+function ConnectCard({users, handleIdxChange, idx}) {
+    console.log(users)
+    let user = users[idx]
   return (
     <>
         <div className="connect-card-nameplate">
-            <h1>{user.gamertag}</h1>
+            <h1>{user && user.name}</h1>
         </div>
-        <div className="connect-card-body">
-            {user.bio ? 
+         <div className="connect-card-body">
+            {user && user.bio ? 
                     <p>{user.bio}</p> : 
                     <p>This user has not yet set up a bio</p>
             }           </div>
         <div className="connect-card-footer">
-            {user.games ? 
+            {user && user.games ? 
                 <h3>Games: {user.games}</h3> : 
                 <h3>Games: N/A</h3>
             }   
                 &nbsp; <h3>|</h3> &nbsp; 
-            {user.rating ?
-                <h3>Rating: {user.rating}</h3> :
+            {user && user.rating ?
+                <h3>Rating: {user && user.rating}</h3> :
                 <h3>Rating: N/A</h3>
             }
+            <button onClick={handleIdxChange}>Connect!</button>
+            <button onClick={handleIdxChange}>Not Interested</button>
         </div>
     </>
   );
