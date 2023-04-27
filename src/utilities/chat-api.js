21056d8ce55c8c016e6113f88e4c7607e7c79f91
joshuapent <joshdavidpent@gmail.com>
2023-04-27 09:40:10 -0500

    Added chat utilities

--- src/utilities/chat-api.js
+++ src/utilities/chat-api.js
@@ -1,10 +1,14 @@
 import { sendRequest } from "./send-request";
 const BASE_URL = '/api/chat';
 
-export async function getUsers() {
+export async function getChat() {
     return sendRequest(`${BASE_URL}/all`)
 }
 
-export async function login(credentials) {
-    return sendRequest(`${BASE_URL}/login`, "POST", credentials);
+export async function connectUsers(credentials) {
+    return sendRequest(`${BASE_URL}/create`, "POST", users);
+}
+
+export async function disconnectUsers(credentials) {
+    return sendRequest(`${BASE_URL}/delete`, "POST", users);
 }
