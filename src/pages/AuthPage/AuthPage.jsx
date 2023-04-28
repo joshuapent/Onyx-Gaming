f91514822cf53c5e85773c6f3fb683e99bec433a
Piquant95 <piquant95@yahoo.com>
2023-04-28 14:13:33 -0400

    changed text color on form

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,10 +1,14 @@
 import LoginForm from '../../components/LoginForm/LoginForm';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import "../AuthPage/float.mp4";
 
 
 export default function AuthPage({setUser}) {
   return (
     <main className='auth'>
+      <video autoPlay loop muted playsInline type="video/mp4">
+        <source src='../Authpage/float.mp4' type='video/mp4'/>
+      </video>
       <h1>Onyx Gaming</h1>
       <div>
       <SignUpForm setUser={setUser}/>
