ebec32ed1ecf0e75b2c8aa6a0a05efbb2a54a1e6
Piquant95 <piquant95@yahoo.com>
2023-04-28 11:29:38 -0400

    reformatting login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -28,7 +28,7 @@ export default function LoginForm({ setUser }) {
   }
 
   return (
-    <div>
+    <div className='box'>
       <div className="form-container">
         <form autoComplete="off" onSubmit={handleSubmit}>
           <label>Email</label>
