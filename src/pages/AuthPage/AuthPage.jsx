067cd59c800d7e716fc5c6780f49a6df453c5301
Piquant95 <piquant95@yahoo.com>
2023-04-28 13:29:02 -0400

    auth forms complete

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -5,7 +5,7 @@ import SignUpForm from '../../components/SignUpForm/SignUpForm';
 export default function AuthPage({setUser}) {
   return (
     <main className='auth'>
-      <h1>AuthPage</h1>
+      <h1>Onyx Gaming</h1>
       <div>
       <SignUpForm setUser={setUser}/>
       </div>
