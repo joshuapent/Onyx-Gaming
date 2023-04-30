5ff89e1867e863c3937fb890ac7ff668b5471115
Piquant95 <piquant95@yahoo.com>
2023-04-30 03:30:38 -0400

    Connections page satisfactory

--- src/components/Invitations/Invitations.jsx
+++ src/components/Invitations/Invitations.jsx
@@ -1,17 +1,20 @@
 import "./Invitations.css";
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 function Invitations({ user }) {
     const invites = [{name: 'john'}]
 
   return (
     <div className="ChatPage">
-      <h1 className="inviteh1">Invitations (number)</h1>
+      <h1 className="inviteh1"><PersonAddAltIcon fontSize="large"/>&nbsp; Invitations (number)</h1>
+      <hr className="line"/>
       <ul className="inviteul">
           {invites.map((invite, idx) => {
             return (
-              <li key={invite + idx}>
-                <span>{invite.name}: </span>
-                <button>Accept</button>
+              <li className="inviteli" key={invite + idx}>
+                <span className="invitespan">{invite.name}: </span>
+                <button className="invitebtn"><CheckCircleOutlineIcon fontSize="large"/> Accept</button>
               </li>
             );
           })}
